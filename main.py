12987d80ba731b38a9a4958d63cee476a4785acf
Мурадов Мурад Ровшан <Murad_uv46@itstep.edu.az>
2022-12-13 17:56:46 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,85 @@
+#
+# a=int((input("a=")))
+# b=int((input("b=")))
+#
+#
+#
+# while a<b:
+#
+#     if (a % 2 == 0):
+#          a+= 1
+#     else:
+#        a  += 2
+#
+#     print (a)
+#
+#
+#
+# a=int((input("a=")))
+# b=int((input("b=")))
+
+
+
+
+
+
+
+
+#task2
+
+
+
+
+
+
+# a=int((input("a=")))
+# b=int((input("b=")))
+#
+#
+#
+#
+# while a<b:
+#
+#     if (a % 2 == 0):
+#          a+= 2
+#     else:
+#        a  += 1
+#   print (a)
+
+
+
+
+
+
+# task3
+
+
+a=int((input("a=")))
+b=int((input("b=")))
+
+d=a
+e=b
+
+while a<=b:
+
+    print(a)
+    a += 1
+
+a = d
+b = e
+print ("--------------------------------------------------------------------------")
+
+while a <= b:
+    print (b)
+    b -= 1
+
+d=a
+e=b
+
+
+print("--------------------------------------------------------------------------")
+
+while a <= b:
+    if a % 7 == 0:
+        print (a)
+print (a)
